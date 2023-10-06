90db7f9e2bba61086737d9097a7b3a599ddc006f
kenmaready <ken@maready.com>
2023-10-05 20:39:22 -0400

    Add explosion, flashing, immobility sequence to Player upon collision with asteroid

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,8 +1,10 @@
+using System.Collections;
 using UnityEngine;
 
 public class Player : MonoBehaviour {
 
     [SerializeField] private Bullet bulletPrefab;
+    [SerializeField] ParticleSystem explosion;
 
     private Rigidbody2D _rigidbody;
     private BulletSpawnPoint _bulletSpawnPoint;
@@ -10,6 +12,7 @@ public class Player : MonoBehaviour {
     private float turnSpeed = .15f;
     private bool _thrusting;
     private float _turnDirection;
+    private bool _frozen = false;
 
     private void Awake() {
         _rigidbody = GetComponent<Rigidbody2D>();
@@ -17,6 +20,8 @@ public class Player : MonoBehaviour {
     }
 
     private void Update() {
+        if (_frozen) return;
+
         _thrusting = (Input.GetKey(KeyCode.UpArrow));
 
         if (Input.GetKey(KeyCode.LeftArrow)) {
@@ -43,6 +48,38 @@ public class Player : MonoBehaviour {
     private void Fire() {
         Bullet bullet = Instantiate(this.bulletPrefab, _bulletSpawnPoint.transform.position, this.transform.rotation);
         bullet.Fire(this.transform.up);
+    }
+
+    private void OnCollisionEnter2D(Collision2D other) {
+        if (other.gameObject.tag == "Asteroid") {
+            FreezeControls();
+            Explode();
+        }
+    }
+
+    private void FreezeControls() {
+        _thrusting = false;
+        _frozen = true;
+    }
+
+    private void Explode() {
+        StartCoroutine(FlashRed());
+        Instantiate(this.explosion, this.transform.position, Quaternion.identity);
+        Destroy(this.gameObject, 4.0f);
+    }
+
+    private IEnumerator FlashRed() {
+        SpriteRenderer renderer = this.GetComponent<SpriteRenderer>();
+        renderer.color = Color.red;
+
+        while (true) {
+            yield return new WaitForSeconds(0.25f);
+            renderer.color = Color.white;
+
+            yield return new WaitForSeconds(0.25f);
+            renderer.color = Color.red;
+        }
+
     }
 
 }
